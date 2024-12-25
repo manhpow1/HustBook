011977777d9d8a6bbcfca84dfa0c15c2b8c3f125
manhpow1 <dd.manh02@gmail.com>
2024-12-25 22:50:53 +0700

    fix

--- client/vite.config.js
+++ client/vite.config.js
@@ -13,7 +13,8 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'lib/utils': fileURLToPath(new URL('./src/lib/utils', import.meta.url)),
     }
   },
   test: {
