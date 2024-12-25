007a02df58e5bd4f3dd7a47571ebe385ed6703ad
manhpow1 <dd.manh02@gmail.com>
2024-12-25 22:35:17 +0700

    import

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import autoprefixer from 'autoprefixer'
 import tailwind from 'tailwindcss'
 import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   css: {
