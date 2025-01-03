575fad1d12137e082d908a913f7d6a1391139124
manhpow1 <dd.manh02@gmail.com>
2025-01-03 18:10:24 +0700

    fix

--- client/src/utils/sanitize.js
+++ client/src/utils/sanitize.js
@@ -1,6 +1,7 @@
 import DOMPurify from 'dompurify';
 
 export const sanitizeInput = (input) => {
+    if (typeof input !== 'string') return output;
     return DOMPurify.sanitize(input, {
         ALLOWED_TAGS: [],
         ALLOWED_ATTR: [],
@@ -8,5 +9,6 @@ export const sanitizeInput = (input) => {
 };
 
 export const sanitizeOutput = (output) => {
+    if (typeof output !== 'string') return output; 
     return DOMPurify.sanitize(output);
 };
